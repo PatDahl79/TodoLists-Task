f49ff7028972cbbba6133d1a28e400bbb69a4f47
PatDahl79 <pattaravarat.dahl@chasacademy.se>
2024-04-28 00:09:08 +0200

    28/4 summit
    Summit

---
+++ todolists-task/src/components/TodoLists.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function TodoLists() {
+    return (
+        <div>
+            <h1>Welcome to Todo-Lists App</h1>
+            <p>This is the home page of our Todo-Lists App.</p>
+        </div>
+    );
+}
+
+export default TodoLists;
